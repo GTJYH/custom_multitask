359d9b0a7b94570ff2e8a1f5d99b9d2894fc0b81
GTJYH <41416789+GTJYH@users.noreply.github.com>
2025-08-14 22:14:27 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from analysis.taskset import analyze_saccade_taskspace
-
-# 分析任务空间（自动包含delay epoch）
-tsa, h_trans = analyze_saccade_taskspace(
-    model_path="checkpoints/random_experiment_20250814_104210/model/model_final.pth",
-    epochs=None,  # 使用自动检测，会包含delay epoch
-    dim_reduction_type='PCA'
-)
